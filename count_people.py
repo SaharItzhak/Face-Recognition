4aa79f6c09f08ed31e8510ed0b05f73f03a5e37d
Xala <56874379+SaharItzhak@users.noreply.github.com>
2020-11-15 12:33:48 +0200

    Add files via upload

---
+++ count_people.py
@@ -0,0 +1,11 @@
+""""Counts number of people in group photo"""
+
+import face_recognition
+
+image = face_recognition.load_image_file('./images/Group1.jpg')
+face_locations = face_recognition.face_locations(image)
+
+# Array of coords of each face
+# print(face_locations)
+
+print(f'There are {len(face_locations)} people in this image')
